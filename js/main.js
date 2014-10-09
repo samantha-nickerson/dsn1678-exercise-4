bf758ca29583052acf99ee6279dcd87895da6311
samantha-nickerson <nick0046@algonquinlive.com>
2014-10-09 10:50:01 -0400

    added scrolling effects to ship section

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ var $dipperSection = $('.dipper-section');
 var $dipper = $('.dipper');
 var $shuttleSection = $('.shuttle-section');
 var $shuttle = $('.shuttle');
+var $shipSection = $('.ship-section');
+var $ships1 = $('.ships-1');
+var $ships2 = $('.ships-2');
 
 
 $win.on('scroll', function () {
@@ -12,6 +15,8 @@ $win.on('scroll', function () {
 
 	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
 	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
+	
+	$shipSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
 });
 
 $dipperSection.waypoint(function () {
@@ -19,7 +24,11 @@ $dipperSection.waypoint(function () {
 }, { offset: '50%' });
 
 $shuttleSection.waypoint(function () {
-	var scrollPos = $win.scrollTop();
-
 	$shuttle.addClass('js-shuttle-fade');
+}, { offset: '50%' });
+
+$shipSection.waypoint(function () {
+	$ships1.addClass('js-ships-1-fade');
+
+	$ships2.addClass('js-ships-2-fade');
 }, { offset: '50%' });
